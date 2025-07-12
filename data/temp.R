493f05b6de996e695801237a27289edce15e7f63
Daniel Burkhalter <108594996+dan-burk@users.noreply.github.com>
2025-07-12 18:48:54 -0500

    Updated R Script to add schools.json

--- data/temp.R
+++ data/temp.R
@@ -2,6 +2,8 @@
 
 df_points_boys_list <- readRDS("C:/Users/Daniel/OneDrive - South Dakota State University - SDSU/Documents/School/STAT 651/XC Ranking/XC Ranking App/sdxc-basic/df_points_boys_list.rds")
 df_points_girls_list <- readRDS("C:/Users/Daniel/OneDrive - South Dakota State University - SDSU/Documents/School/STAT 651/XC Ranking/XC Ranking App/sdxc-basic/df_points_girls_list.rds")
+schools <- readRDS("C:/Users/Daniel/OneDrive - South Dakota State University - SDSU/Documents/School/STAT 651/XC Ranking/XC Ranking App/sdxc-basic/list_schools.rds")
+
 library(dplyr)
 for(i in 1:7) {
   # Boys data
@@ -18,3 +20,9 @@ for(i in 1:7) {
     pretty = TRUE
   )
 }
+
+  jsonlite::write_json(
+    schools, 
+    paste0("schools.json"),
+    pretty = TRUE
+  )
